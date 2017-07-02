d6f355c4b5be690624c81a08a97a8fd7a2ee748b
samover <samuel.overloop@gmail.com>
2017-07-02 22:49:20 +0200

    NEB-39: Adds model tests

--- src/models/author.model.ts
+++ src/models/author.model.ts
@@ -1,8 +1,22 @@
-import { Table, Column, Model, HasMany } from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  HasMany,
+  PrimaryKey,
+  DataType,
+  Default,
+  IsUUID } from 'sequelize-typescript';
 import Book from './book.model';
 
-@Table({ tableName: 'authors' })
+@Table({ tableName: 'authors', timestamps: true })
 export default class Author extends Model<Author> {
+  @IsUUID(4)
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column
+  id: string;
+
   @Column
   firstName: string;
 
