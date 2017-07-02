6f12c27b765a5fa5334af9e1a8dc267e4242a614
samover <samuel.overloop@gmail.com>
2017-07-02 22:49:20 +0200

    NEB-39: Adds model tests

---
+++ __tests__/models/author.model.test.ts
@@ -0,0 +1,48 @@
+/**
+ * author.model.test.ts
+ * __tests__/models
+ *
+ * Created by samover on 02/07/2017.
+ */
+
+import { Author } from '../../src/models';
+
+describe('models/Author', () => {
+  let author;
+
+  beforeAll(async () => {
+    author = await Author.create<Author>();
+  });
+
+  describe('Instance', () => {
+    it('of Author', () => {
+      expect(author).toBeInstanceOf(Author);
+    });
+  });
+
+  describe('It has the attributes', () => {
+    it('id', () => {
+      expect(author.dataValues).toHaveProperty('id');
+    });
+    it('firstName', () => {
+      expect(author.dataValues).toHaveProperty('firstName');
+    });
+    it('lastName', () => {
+      expect(author.dataValues).toHaveProperty('lastName');
+    });
+    it('updatedAt', () => {
+      expect(author.dataValues).toHaveProperty('updatedAt');
+    });
+    it('createdAt', () => {
+      expect(author.dataValues).toHaveProperty('createdAt');
+    });
+  });
+
+  describe('It has', () => {
+    it('has many Books', () => {
+      expect(author.setBooks).toBeInstanceOf(Function);
+      expect(author.getBooks).toBeInstanceOf(Function);
+    });
+  });
+});
+
--- src/models/book.model.ts
+++ src/models/book.model.ts
@@ -1,8 +1,23 @@
-import { Table, Column, Model, BelongsTo, ForeignKey } from 'sequelize-typescript';
+import {
+  Table,
+  Column,
+  Model,
+  BelongsTo,
+  PrimaryKey,
+  DataType,
+  Default,
+  ForeignKey,
+  IsUUID } from 'sequelize-typescript';
 import Author from './author.model';
 
-@Table({ tableName: 'books' })
+@Table({ tableName: 'books', timestamps: true })
 export default class Book extends Model<Book> {
+  @IsUUID(4)
+  @PrimaryKey
+  @Default(DataType.UUIDV4)
+  @Column
+  id: string;
+
   @Column
   title: string;
 
